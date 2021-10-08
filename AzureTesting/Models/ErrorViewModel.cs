1d43e3a1c4235b53841d8132fe66792ec7b98ddc
Piyush305793 <piyushghanmule1999@gmail.com>
2021-10-08 13:05:51 +0530

    Add project files.

---
+++ AzureTesting/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureTesting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
